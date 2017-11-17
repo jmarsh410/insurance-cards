c06559a36fd444746a318955b51adbfbddbe6ab4
Jered Marshall <jmarsh410@gmail.com>
2017-11-16 21:08:46 -0600

    edit boilerplates and add content for insuranc card project

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,43 +1,25 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Footer.css';
-import Link from '../Link';
+import s from './Footer.scss';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/">
-            Home
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/admin">
-            Admin
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">
-            Privacy
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">
-            Not Found
-          </Link>
-        </div>
+/**
+ * Footer component. Includes name and attribution for the generator
+ */
+function Footer() {
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <span>Made by Jered Marshall with </span>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/kriasoft/react-starter-kit"
+        >
+          React Starter Kit
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withStyles(s)(Footer);
