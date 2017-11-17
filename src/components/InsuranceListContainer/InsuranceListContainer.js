2520cea6a63c2e890797d16b0f29312d3fbf16ad
Jered Marshall <jmarsh410@gmail.com>
2017-11-17 12:12:06 -0600

    edits to the list container

--- src/components/InsuranceListContainer/InsuranceListContainer.js
+++ src/components/InsuranceListContainer/InsuranceListContainer.js
@@ -1,19 +1,21 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './InsuranceListContainer.scss';
 import Select from '../Select';
 import InsuranceCard from '../InsuranceCard';
+/* eslint-enable */
 
 /**
  * associate select terms to their fields in the json data
  */
 const SEARCH_TERMS = {
-  match: 'card_order_best_match',
-  price: 'card_order_price',
-  rating: 'stars',
-  atoz: 'card_order_a_to_z',
-  ztoa: 'card_order_z_to_a',
+  match: ['card_order_best_match', 0],
+  price: ['card_order_price', 0],
+  rating: ['stars', 1],
+  atoz: ['card_order_a_to_z', 0],
+  ztoa: ['card_order_z_to_a', 0],
 };
 
 /**
@@ -28,21 +30,44 @@ class InsuranceListContainer extends Component {
   static getMins(ms) {
     return (ms / 1000 / 60).toFixed(2); // ms -> sec -> min
   }
-  // map over card objects and pass data to insurance card components
-  static getCards(array) {
-    return array.map(card => (
-      <li key={card.name}>
-        <InsuranceCard data={card} />
-      </li>
-    ));
+  static addToggleProperty(array) {
+    return array.map(obj => Object.assign({ isOpen: false }, obj));
   }
   constructor(props) {
     super(props);
     this.state = {
       sort: 'match',
+      cards: InsuranceListContainer.addToggleProperty(
+        this.props.data.carrier_cards,
+      ),
     };
     // create bound version of handleSortChange
     this.handleSortChange = this.handleSortChange.bind(this);
+    this.handleCardClick = this.handleCardClick.bind(this);
+  }
+  // map over card objects and pass data to insurance card components
+  getCards(array) {
+    return array.map(card => (
+      <li key={card.name}>
+        <InsuranceCard clickHandler={this.handleCardClick} data={card} />
+      </li>
+    ));
+  }
+  handleCardClick(e) {
+    e.preventDefault();
+    const name = e.target
+      .closest('.card-container')
+      .getElementsByClassName('card-name')[0].innerText;
+    this.setState(prevState => {
+      const newState = Object.assign({}, prevState);
+      for (let i = 0; i < prevState.cards.length; i += 1) {
+        if (newState.cards[i].name === name) {
+          newState.cards[i].isOpen = !newState.cards[i].isOpen;
+          break;
+        }
+      }
+      return { cards: newState.cards };
+    });
   }
   // change the sort state when the sort select's value changes
   handleSortChange(e) {
@@ -51,22 +76,34 @@ class InsuranceListContainer extends Component {
   }
   // arrange cards based off of the current term stored in state
   sortCards() {
-    return this.props.data.carrier_cards
-      .sort((a, b) => {
-        const term = SEARCH_TERMS[this.state.sort];
-        const termA = a[term] && typeof a[term] === 'number' ? a[term] : 0;
-        const termB = b[term] && typeof b[term] === 'number' ? b[term] : 0;
-        if (termA > termB) {
-          return 1;
-        } else if (termA > termB) {
-          return -1;
-        }
-        return 0;
-      })
-      .reverse();
+    const sortedCards = this.state.cards.sort((a, b) => {
+      const term = SEARCH_TERMS[this.state.sort][0];
+      const termA = a[term] && typeof a[term] === 'number' ? a[term] : 0;
+      const termB = b[term] && typeof b[term] === 'number' ? b[term] : 0;
+      if (termA > termB) {
+        return 1;
+      } else if (termA < termB) {
+        return -1;
+      }
+      // they are equal, sort them by name
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      // names and terms are equal. QUITE unusual
+      return 0;
+    });
+    if (SEARCH_TERMS[this.state.sort][1] === 1) {
+      sortedCards.reverse();
+    }
+    return sortedCards;
   }
   render() {
-    const cards = InsuranceListContainer.getCards(this.sortCards());
+    const cards = this.getCards(this.sortCards());
     return (
       <div className={s.root}>
         <div className={s.container}>
--- src/components/InsuranceListContainer/InsuranceListContainer.test.js
+++ src/components/InsuranceListContainer/InsuranceListContainer.test.js
@@ -1,21 +1,34 @@
 /* eslint-env jest */
 /* eslint-disable padded-blocks, no-unused-expressions */
 
+/* eslint-disable */
 import React from 'react';
 import { configure, mount } from 'enzyme';
-// import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 import InsuranceListContainer from './InsuranceListContainer';
 import InsuranceCard from '../InsuranceCard';
 import Select from '../Select';
 import data from '../../data/carrier_cards.json';
+/* eslint-enable */
 
 configure({ adapter: new Adapter() });
 
 describe('InsuranceListContainer', () => {
   test('converts ms to mins string', () => {
     expect(InsuranceListContainer.getMins(60000)).toBe('1.00');
+  });
+
+  test('getMins returns NaN with non-number arguments', () => {
+    expect(InsuranceListContainer.getMins('hi')).toBe('NaN');
+  });
+
+  test('adds toggle property to objects in array', () => {
+    const newArray = InsuranceListContainer.addToggleProperty([
+      { test: 'test' },
+      { test: 'test' },
+    ]);
+    expect(newArray.every(el => el.isOpen === false)).toBe(true);
   });
 
   test('creates a select', () => {
@@ -36,20 +49,16 @@ describe('InsuranceListContainer', () => {
     expect(wrapper.find(Select).find('option').length).toBe(5);
   });
 
-  // test('changing the select value causes a state change', () => {
-  //   sinon.spy(InsuranceListContainer.prototype, 'handleSortChange');
-  //   const wrapper = mount(
-  //     <App context={{ insertCss: () => {}, fetch: () => {} }}>
-  //       <InsuranceListContainer data={data} />
-  //     </App>,
-  //   );
-  //   wrapper.find(Select).find('select').value = 'price';
-  //   expect(InsuranceListContainer.prototype.handleSortChange.calledOnce).toBe(
-  //     true,
-  //   );
-  // });
+  test('creates no cards with empty array', () => {
+    const wrapper = mount(
+      <App context={{ insertCss: () => {}, fetch: () => {} }}>
+        <InsuranceListContainer data={{ carrier_cards: [] }} />
+      </App>,
+    );
+    expect(wrapper.find(InsuranceCard).length).toBe(0);
+  });
 
-  test('creates cards', () => {
+  test('creates cards when provided', () => {
     const wrapper = mount(
       <App context={{ insertCss: () => {}, fetch: () => {} }}>
         <InsuranceListContainer data={data} />
